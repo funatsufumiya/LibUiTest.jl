dc5363f2029df344aeaec2bdaeeff9f01b8cc015
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-04-12 18:00:37 +0900

    feat: add msgbox button

--- src/LibUiTest.jl
+++ src/LibUiTest.jl
@@ -16,6 +16,7 @@ end
 
 const uiWindow = Ptr{Cvoid}
 const uiControl = Ptr{Cvoid}
+const uiButton = Ptr{Cvoid}
 const OnClosingFuncType = Ptr{Cvoid}
 const UserData = Ptr{Cvoid}
 
@@ -24,17 +25,29 @@ uiFreeInitError=(err)->ccall((:uiFreeInitError,libui_lib),Cvoid,(Cstring,),err)
 uiNewWindow=(title, width, height, hasMenuBar)->ccall((:uiNewWindow,libui_lib),uiWindow,(Cstring,Cint,Cint,Cint),title,width,height,hasMenuBar)
 uiNewGrid=()->ccall((:uiNewGrid,libui_lib),Ptr{Cvoid},(),)
 uiControlShow=(c)->ccall((:uiControlShow,libui_lib),Cvoid,(uiControl,),c)
+uiGridSetPadded=(grid, pad)->ccall((:uiGridSetPadded,libui_lib),Cvoid,(uiControl,Cint),grid,pad)
+uiNewButton=(s)->ccall((:uiNewButton,libui_lib),uiControl,(Cstring,),s)
+uiWindowSetChild=(win, c)->ccall((:uiWindowSetChild,libui_lib),Cvoid,(uiWindow, uiControl,),win,c)
 uiMain=()->ccall((:uiMain,libui_lib),Cvoid,(),)
 uiQuit=()->ccall((:uiQuit,libui_lib),Cvoid,(),)
 uiUninit=()->ccall((:uiUninit,libui_lib),Cvoid,(),)
 uiWindowOnClosing=(win, onClosing, data)->ccall((:uiWindowOnClosing,libui_lib),Cvoid,(Ptr{Cvoid},OnClosingFuncType,UserData),win,onClosing,data)
+uiButtonOnClicked=(c, func, data)->ccall((:uiButtonOnClicked,libui_lib),Cvoid,(Ptr{Cvoid},Ptr{Cvoid},UserData),c,func,data)
+uiMsgBox=(win,msg1,msg2)->ccall((:uiMsgBox,libui_lib),Cvoid,(uiWindow, Cstring, Cstring),win,msg1,msg2)
 
 global already_quitted = false
+global w::uiWindow
 
 function onClose(w::uiWindow, data::UserData)::Cint
     global already_quitted = true
     uiQuit()
     return 1
+end
+
+function onMsgBoxClick(b::uiButton, data::UserData)
+	uiMsgBox(w,
+	    "This is a normal message box.",
+		"More detailed information can be shown here.")
 end
 
 function main()
@@ -47,13 +60,21 @@ function main()
         uiFreeInitError(err)
     end
 
-    w = uiNewWindow("Hello World", 300, 300, 0)
-    # grid = uiNewGrid()
+    onClosing = @cfunction(onClose, Cint, (uiWindow, UserData))
+    onMsgBoxClicked = @cfunction(onMsgBoxClick, Cvoid, (uiControl, UserData))
+
+    global w = uiNewWindow("Hello World", 300, 300, 0)
+    grid = uiNewGrid()
+    uiGridSetPadded(grid, 1)
+
+    button = uiNewButton("Message Box")
+    uiButtonOnClicked(button, onMsgBoxClicked, C_NULL);
+
+    # uiWindowSetChild(w, grid);
+    uiWindowSetChild(w, button);
 
     # println("window: ", w)
     # println("grid: ", grid)
-
-    onClosing = @cfunction(onClose, Cint, (uiWindow, UserData))
 
     uiWindowOnClosing(w, onClosing, C_NULL);
     uiControlShow(w)
